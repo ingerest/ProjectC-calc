cc4f8c28070127326eba7b638f3195e474a0c69e
ltw070 <168509093+ltw070@users.noreply.github.com>
2025-03-26 17:17:23 +0900

    getSum
    곱을 더하기로 변경하였습니다.

--- Project20/main.cpp
+++ Project20/main.cpp
@@ -4,13 +4,21 @@ class Cal
 {
 public:
 	//ÀÌ°÷¿¡ Á¦ÀÛ
-	int getGop(int a, int b){return a*b;}
+	int getSum(int a, int b){return a+b;}
 };
 
 //Å×ÄÉ Ãß°¡
 TEST(t1, t2)
 {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, TSUM)
+{
+	Cal cal;
+	int expected = 6;
+	int actual = cal.getSum(3,3);
+	EXPECT_EQ(expected, actual);
 }
 
 int main()
